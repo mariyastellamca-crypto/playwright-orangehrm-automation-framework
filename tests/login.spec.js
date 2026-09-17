e9cffdcf21b8ee380a776001b7e451c02318d252
mariyastellamca-crypto <mariyastellamca@gmail.com>
2026-09-17 16:51:17 +0530

    Revert "Code Refactor"
    This reverts commit f48c6c598beadba1ae8d79a027d7a15ece1274e8.

---
+++ tests/login.spec.js
@@ -0,0 +1,45 @@
+import { loginData } from '../test-data/loginData';
+import {test , expect } from '../fixtures/baseFixture.js';
+
+test('Login page verification' , async({page , loginPage}) => {
+    await loginPage.goto();
+    await expect(loginPage.logoImage).toBeVisible();
+    await expect(loginPage.usernameLabel).toBeVisible();
+    await expect(loginPage.passwordLabel).toBeVisible();
+    await expect(loginPage.submit).toBeEnabled();
+
+});
+
+test('Valid Login' , async({page , loginPage , gotoPage}) => {
+
+    await loginPage.login(loginData.validUser.username , loginData.validUser.password);
+    await expect(page).toHaveURL(/dashboard/);
+
+
+});
+
+test('Should display error message for invalid credentials' , async({page , loginPage , gotoPage}) => {
+
+    await loginPage.login(loginData.invalidUser.username , loginData.invalidUser.password);
+    await expect(loginPage.invalidCredError).toHaveText('Invalid credentials');
+});
+
+test('Should display validation message when username is empty' , async({page ,loginPage , gotoPage})=> {
+    await loginPage.login('' , loginData.validUser.password);
+    await expect(loginPage.blankFieldError).toContainText('Required');
+
+});
+
+test('Should display validation message when password is empty' , async({page , loginPage , gotoPage})=> {
+    await loginPage.login(loginData.validUser.username , '');
+    await expect(loginPage.blankFieldError).toContainText('Required');
+
+});
+
+test('Logout' , async({page, loginPage, loggedInPage}) => {
+
+    await loginPage.logout();
+    await expect(loginPage.loginScreen).toBeVisible;
+
+});
+
